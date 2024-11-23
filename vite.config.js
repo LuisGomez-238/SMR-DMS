3a4ebf43bf3d38572d8a6b1525e62c1237b9f2df
Gurpartap Grewal <luis.gomez.cohort238@gmail.com>
2024-11-22 17:17:18 -0800

    deploying to vercel

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  root: path.resolve(__dirname, ''),
   build: {
+    outDir: 'dist',
+    emptyOutDir: true,
     rollupOptions: {
+      input: path.resolve(__dirname, 'index.html'),
       output: {
         manualChunks: {
           // Vendor chunk for major libraries
