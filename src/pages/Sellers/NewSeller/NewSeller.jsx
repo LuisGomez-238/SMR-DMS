e3e861c9b637f87d470a54c88b0f68a45667fd64
Gurpartap Grewal <luis.gomez.cohort238@gmail.com>
2024-11-22 17:28:12 -0800

    fixing imports

--- src/pages/Sellers/NewSeller/NewSeller.jsx
+++ src/pages/Sellers/NewSeller/NewSeller.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { sellersService } from '../../../services/sellersService';
+import { sellersService } from '../../../firebase/services/sellersService';
 import Navbar from '../../components/Navbar/Navbar';
 import './NewSeller.css';
 
